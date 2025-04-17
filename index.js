a35a1e724f6491642a5b29363eaa7a4badf38357
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-04-18 03:55:34 +0530

    Created the login and signup ui

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Server is running!');
 });
+
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+  });
+  
+  app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  });  
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
