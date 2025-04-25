9d4961d077a1f477321f93de3aec866eb76f516e
Sarthak sharma <sharma.sarthak222872@gmail.com>
2025-04-25 11:01:44 +0530

    Fixed the space.ejs spaces rendering

--- routes/spaceRouter.js
+++ routes/spaceRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { createSpace } from '../controllers/space.js';
+import prisma from '../prisma/client.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render("spaces");
+router.get('/', async(req, res) => {
+  const spaces = await prisma.space.findMany();
+
+  res.render("spaces", { spaces });
 });
 
 router.post('/', createSpace);
